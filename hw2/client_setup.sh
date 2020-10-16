ef438cb61e93ebdb9a457ce3d47bb7ec7b7c609c
Federickg23 <Federickg23@gmail.com>
2020-10-16 05:03:19 +0000

    Fixed to have web_client instead of web_server

--- hw2/client_setup.sh
+++ hw2/client_setup.sh
@@ -26,14 +26,13 @@ openssl ca -config $HOME/ca/intermediate/openssl.cnf \
       -in client/csr/web_client.csr.pem \
       -out client/certs/web_client.cert.pem
 
-
 chmod 444 client/certs/web_client.cert.pem
 
 openssl x509 -noout -text \
-      -in client/certs/web_server.cert.pem
+      -in client/certs/web_client.cert.pem
 
 openssl verify -CAfile intermediate/certs/ca-chain.cert.pem \
-      client/certs/web_server.cert.pem
+      client/certs/web_client.cert.pem
 
 openssl s_client -cert client/certs/web_client.cert.pem \
       -key client/private/web_client.key.pem \
